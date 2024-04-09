bee67217890abea88d7fdc016c70cffe147a374a
Jueuunn7 <wndmsrla1234@gmail.com>
2024-04-09 13:31:01 +0900

    Error Fix

--- common/views.py
+++ common/views.py
@@ -2,7 +2,7 @@ from .serilizers import CustomerSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from others.Jwt.JwtHandler import JwtHandler
+from utils.Jwt.JwtHandler import JwtHandler
 from django.contrib.auth import authenticate
 from django.contrib.auth.hashers import make_password
 
