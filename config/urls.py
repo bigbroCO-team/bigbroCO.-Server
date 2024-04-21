f8f9c0586bbc5df31dd26dd176605c58d16e040e
Jueuunn7 <wndmsrla1234@gmail.com>
2024-04-21 15:49:28 +0900

    Edit swagger title

--- config/urls.py
+++ config/urls.py
@@ -23,7 +23,7 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Your API",
+        title="bigbro API",
         default_version='v1',
         description="bigbro API description",
         terms_of_service="https://www.google.com/policies/terms/",
