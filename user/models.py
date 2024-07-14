8cfc8806a02cd4f45ed0dee5a6b2c89146ba2f92
Jueuunn7 <wndmsrla1234@gmail.com>
2024-07-15 01:54:23 +0900

    Create user app

---
+++ user/models.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+# Create your models here.
+class User(AbstractUser):
+
+    email = models.EmailField(
+        unique=True,
+        null=False
+    )
+
+    signature = models.CharField(
+        max_length=4,
+        null=False
+    )
+
+    phone = models.CharField(
+        max_length=12,
+        null=False
+    )
