e00be84ac8ea5131128e92ed9df924ddfa650cf0
Jueuunn7 <wndmsrla1234@gmail.com>
2024-07-16 12:09:33 +0900

    Edit :: file root

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -159,8 +159,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_ROOT = 'images/'
+STATIC_URL = '/'
+MEDIA_ROOT = '/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
