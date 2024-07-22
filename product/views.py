d7ec02711f2e949c7a209f0d578423cd73f705a9
Jueuunn7 <wndmsrla1234@gmail.com>
2024-07-22 09:44:54 +0900

    Edit :: product logic

--- product/views.py
+++ product/views.py
@@ -10,13 +10,17 @@ from product.serializers import ProductSerializer
 
 # Create your views here.
 class ProductView(APIView):
-    # authentication_classes = [JWTAuthentication]
-    # permission_classes = [AllowAny]
-
     def get(self, request: object) -> Response:
         products = Product.objects.all()
         serializer = ProductSerializer(instance=products, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        serialized_data = serializer.data
+
+        for product in serialized_data:
+            if not product['open_stock']:
+                for option in product['options']:
+                    del option['stock']
+
+        return Response(serialized_data, status=status.HTTP_200_OK)
 
 
 class ProductAdminView(APIView):
