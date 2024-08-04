2c0f327ee2f6be3b472cb3c5539664dbeff4a1b5
Jueuunn7 <wndmsrla1234@gmail.com>
2024-08-04 23:55:05 +0900

    Remove :: cart testcode

--- cart/tests.py
+++ cart/tests.py
@@ -8,23 +8,4 @@ from utils.utils import testcode_basic_authentication
 
 # Create your tests here.
 class UploadAddressTestCode(APITestCase):
-    obj: Product.objects
-
-    def setUp(self):
-        testcode_basic_authentication(self)
-        self.obj = Product.objects.create(
-            name="test",
-            description="test",
-            open_stock=True,
-        )
-
-    def test_upload_cart(self):
-        url = reverse('cart')
-        data = {
-            "product": self.obj.id,
-            "count": 2,
-            "option": 5
-        }
-
-        res = self.client.post(url, data, format='json')
-        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+    pass
