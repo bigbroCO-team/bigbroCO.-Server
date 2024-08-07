bce5e45b43dfc556199c3b7ca9dab7b87db7f809
Jueuunn7 <wndmsrla1234@gmail.com>
2024-08-08 03:27:46 +0900

    Edit deploy

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM python:3.12
-
-WORKDIR /app
-
-COPY . .
-
-RUN pip install --upgrade pip
-RUN pip install -r requirements.txt
--- config/wsgi.py
+++ config/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.dev')
+env = os.environ.get("env")
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', f'config.settings.{env}')
 
 application = get_wsgi_application()
