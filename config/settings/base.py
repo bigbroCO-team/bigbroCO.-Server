69b37a195347aa584c3fca38c4a572e1c172a806
Jueuunn7 <wndmsrla1234@gmail.com>
2024-08-09 01:06:12 +0900

    Edit :: settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -30,7 +30,7 @@ env = environ.Env()
 SECRET_KEY = os.environ.get('SECRET_KEY', os.urandom(32))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+DEBUG = os.environ.get('DEBUG', False)
 
 ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS'), "127.0.0.1"]
 
