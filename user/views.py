f94d089a0ce7f7f0e1a3cf2e350b7c4c2b364a83
Jueuunn7 <wndmsrla1234@gmail.com>
2024-08-19 10:22:04 +0900

    Edit :: kakao error

--- user/views.py
+++ user/views.py
@@ -49,52 +49,54 @@ class KakaoSignInView(APIView):
 class KakaoSignInCallbackView(APIView):
     @transaction.atomic
     def get(self, request: object) -> Response:
-
-        """
-            Get access token
-        """
-        code = request.GET.get('code')
-        token_url = "https://kauth.kakao.com/oauth/token"
-        token_data = {
-            "grant_type": "authorization_code",
-            "client_id": KAKAO_API_KEY,
-            "redirect_uri": KAKAO_REDIRECT_URI,
-            "code": code,
-            "client_secret": KAKAO_CLIENT_SECRET
-        }
-        token_headers = {
-            "Content-Type": "application/x-www-form-urlencoded"
-        }
-
-        r = requests.post(token_url, data=token_data, headers=token_headers)
-        if r.status_code != 200:
-            return Response(r.json(), status=r.status_code)
-        """
-            Get user info
-        """
-        user_info = requests.get(
-            url="https://kapi.kakao.com/v2/user/me",
-            headers={"Authorization": f"Bearer {r.json().get('access_token')}"}
-        )
-        if user_info.status_code != 200:
-            return Response(user_info.json(), status=user_info.status_code)
-
-        email = user_info.json()["kakao_account"]["email"]
-        user = User.objects.filter(email=email).first()
-        if not user:
+        try:
             """
-            Todo
-            create -> create_user
-            (kakao oauth2)
+                Get access token
             """
-            user = User.objects.create(email=email)
+            code = request.GET.get('code')
+            token_url = "https://kauth.kakao.com/oauth/token"
+            token_data = {
+                "grant_type": "authorization_code",
+                "client_id": KAKAO_API_KEY,
+                "redirect_uri": KAKAO_REDIRECT_URI,
+                "code": code,
+                "client_secret": KAKAO_CLIENT_SECRET
+            }
+            token_headers = {
+                "Content-Type": "application/x-www-form-urlencoded"
+            }
 
-        token = TokenObtainPairSerializer.get_token(user)
+            r = requests.post(token_url, data=token_data, headers=token_headers)
+            if r.status_code != 200:
+                return Response(r.json(), status=r.status_code)
+            """
+                Get user info
+            """
+            user_info = requests.get(
+                url="https://kapi.kakao.com/v2/user/me",
+                headers={"Authorization": f"Bearer {r.json().get('access_token')}"}
+            )
+            if user_info.status_code != 200:
+                return Response(user_info.json(), status=user_info.status_code)
 
-        headers = {
-            "Location": CLIENT_REDIRECT_URL,
-            "access": str(token.access_token),
-            "refresh": str(token)
-        }
+            email = user_info.json()["kakao_account"]["email"]
+            user = User.objects.filter(email=email).first()
+            if not user:
+                """
+                Todo
+                create -> create_user
+                (kakao oauth2)
+                """
+                user = User.objects.create(email=email)
 
-        return Response(headers=headers, status=status.HTTP_302_FOUND)
+            token = TokenObtainPairSerializer.get_token(user)
+
+            headers = {
+                "Location": CLIENT_REDIRECT_URL,
+                "access": str(token.access_token),
+                "refresh": str(token)
+            }
+
+            return Response(headers=headers, status=status.HTTP_302_FOUND)
+        except Exception as e:
+            return Response({"detail": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
