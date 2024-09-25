7c3143656a116f3fe6c4c548d503cfea71c1508d
Jueuunn7 <wndmsrla1234@gmail.com>
2024-09-25 15:59:40 +0900

    Create TokenVerifyView

--- src/app/user/views.py
+++ src/app/user/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+import jwt
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from config.settings.base import SECRET_KEY
+
 
 # Create your views here.
+class TokenVerifyView(APIView):
+    def get(self, request: object) -> Response:
+        header = request.headers.get('Authorization')
+
+        if not header:
+            return Response({'isValidToken': False, 'message': 'Empty token'}, status=status.HTTP_401_UNAUTHORIZED)
+
+        token_type, token = header.split(' ')
+        if token_type.lower() != 'bearer':
+            return Response({'isValidToken': False}, status=status.HTTP_401_UNAUTHORIZED)
+
+        try:
+            jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+            return Response({'isValidToken': True})
+        except Exception as e:
+            return Response({'isValidToken': False, 'message': str(e)}, status=status.HTTP_401_UNAUTHORIZED)
