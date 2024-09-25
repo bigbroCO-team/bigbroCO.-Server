73271a1eab40f44919f96b919bfbe6a2ac89a620
Jueuunn7 <wndmsrla1234@gmail.com>
2024-09-25 17:09:07 +0900

    Add swagger

--- src/app/user/KakaoView.py
+++ src/app/user/KakaoView.py
@@ -1,3 +1,5 @@
+from drf_spectacular.utils import extend_schema_view, extend_schema
+
 from src.app.user.KakaoService import KakaoService
 from django.db import transaction
 from rest_framework.response import Response
@@ -5,7 +7,11 @@ from rest_framework.views import APIView
 from rest_framework import status
 from config.settings.base import KAKAO_REDIRECT_URI, KAKAO_API_KEY
 
-
+@extend_schema_view(
+    get=extend_schema(
+        summary='Kakao Signin API',
+        description='Kakao OAuth login API 입니다. 로그인 성공 시 access, refresh 토큰이 발급됩니다.',
+    ))
 class KakaoSignInView(APIView):
     def get(self, request: object) -> Response:
         headers = {"Location": f"https://kauth.kakao.com/oauth/authorize?response_type=code&client_id={KAKAO_API_KEY}&redirect_uri={KAKAO_REDIRECT_URI}"}
