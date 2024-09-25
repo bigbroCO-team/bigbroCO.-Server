fcdead61621209d4aa7b86d2d347560b6d756f6c
Jueuunn7 <wndmsrla1234@gmail.com>
2024-09-25 17:32:27 +0900

    Edit installed_apps

--- config/settings/base.py
+++ config/settings/base.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.simple_jwt',
     'drf_spectacular',
     'src.app.user'
 ]
