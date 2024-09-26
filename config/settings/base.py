308ae1ce17538ee68d863101c42d4110b54466f0
Jueuunn7 <wndmsrla1234@gmail.com>
2024-09-26 14:25:31 +0900

    Edit renderer

--- config/settings/base.py
+++ config/settings/base.py
@@ -52,6 +52,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
     'EXCEPTION_HANDLER': 'config.exception.Exception.custom_exception_handler',
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
