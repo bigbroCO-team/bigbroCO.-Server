7fab16780edcc86bc2b2b900ea6aa365a06f7453
Jueuunn7 <wndmsrla1234@gmail.com>
2024-09-26 19:16:17 +0900

    E

--- src/app/client/user/KakaoService.py
+++ src/app/client/user/KakaoService.py
@@ -32,7 +32,7 @@ class KakaoService:
         )
 
         if response.status_code != 200:
-            return Response({'a': response.json().get('access_token'), 'b': response.json()}, status=status.HTTP_400_BAD_REQUEST)
+            raise CustomException.KakaoOAuthCodeIsNotValid
         return response.json().get('access_token')
 
     @staticmethod
@@ -44,8 +44,7 @@ class KakaoService:
             }
         )
         if response.status_code != 200:
-            # raise CustomException.KakaoOAuthAccessTokenIsNotValid
-            return Response({'a': access_token, 'b': response.json()}, status=status.HTTP_400_BAD_REQUEST)
+            raise CustomException.KakaoOAuthAccessTokenIsNotValid
         return response.json().get('kakao_account').get('email')
 
     @staticmethod
