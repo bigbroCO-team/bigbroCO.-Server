d67b26b0a32d25ca6fcb7e066f4ca2e2c7e0c3b8
Jueuunn7 <wndmsrla1234@gmail.com>
2024-09-29 23:27:25 +0900

    Disable cloudwatch

--- config/settings/base.py
+++ config/settings/base.py
@@ -171,35 +171,35 @@ boto3_client = boto3.client(
     aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
     region_name=AWS_DEFAULT_REGION
 )
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-
-    'formatters': {
-        'aws': {
-            'format': u"%(asctime)s [%(levelname)-8s] %(funcName)s - %(message)s [%(pathname)s:%(lineno)d]",
-            'datefmt': "%Y-%m-%d %H:%M:%S"
-        }
-    },
-    'handlers': {
-        'aws-handler': {
-            'level': 'INFO',
-            'class': 'watchtower.CloudWatchLogHandler',
-            'boto3_client': boto3_client,
-            'log_group': AWS_LOG_GROUP,
-            'stream_name': AWS_LOG_STREAM,
-            'formatter': 'aws',
-            'use_queues': True,
-        },
-    },
-    'loggers': {
-        'aws-logger': {
-            'level': 'INFO',
-            'handlers': ['aws-handler'],
-            'propagate': True,
-        }
-    }
-}
-
-log = logging.getLogger('default-logger')
+#
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#
+#     'formatters': {
+#         'aws': {
+#             'format': u"%(asctime)s [%(levelname)-8s] %(funcName)s - %(message)s [%(pathname)s:%(lineno)d]",
+#             'datefmt': "%Y-%m-%d %H:%M:%S"
+#         }
+#     },
+#     'handlers': {
+#         'aws-handler': {
+#             'level': 'INFO',
+#             'class': 'watchtower.CloudWatchLogHandler',
+#             'boto3_client': boto3_client,
+#             'log_group': AWS_LOG_GROUP,
+#             'stream_name': AWS_LOG_STREAM,
+#             'formatter': 'aws',
+#             'use_queues': True,
+#         },
+#     },
+#     'loggers': {
+#         'aws-logger': {
+#             'level': 'INFO',
+#             'handlers': ['aws-handler'],
+#             'propagate': True,
+#         }
+#     }
+# }
+#
+# log = logging.getLogger('default-logger')
