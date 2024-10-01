4e019dc545132a10fd46807d5556dfed59b51afb
Jueuunn7 <wndmsrla1234@gmail.com>
2024-10-01 23:21:17 +0900

    Create cart app

--- config/settings/base.py
+++ config/settings/base.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'src.app.client.user',
     'src.app.client.address',
     'src.app.client.product',
+    'src.app.client.cart'
 ]
 
 REST_FRAMEWORK = {
---
+++ src/app/client/cart/views.py
@@ -0,0 +1,28 @@
+from django.db import transaction
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+from src.app.client.cart.models import Cart
+from src.app.client.cart.serializers import CartSerializer
+
+
+# Create your views here.
+class CartView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request: object) -> Response:
+        cart = Cart.objects.filter(user=request.user)
+        serializer = CartSerializer(cart, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @transaction.atomic
+    def post(self, request: object) -> Response:
+        serializer = CartSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(user=request.user)
+        return Response(status=status.HTTP_200_OK)
+
