50ecaed86800992359671343188cbc6a3b447a67
Jueuunn7 <wndmsrla1234@gmail.com>
2024-10-08 00:47:39 +0900

    Edit base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -27,7 +27,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, f'.env.{env}'))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(int(os.environ.get('DEBUG')))
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
 
