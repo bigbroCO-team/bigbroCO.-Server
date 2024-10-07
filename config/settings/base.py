dd17c9556533c4a404d8a4c5ded5b6b8af733505
Jueuunn7 <wndmsrla1234@gmail.com>
2024-10-08 00:49:27 +0900

    Edit base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -24,12 +24,12 @@ environ.Env.read_env(os.path.join(BASE_DIR, f'.env.{env}'))
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', os.urandom(24).hex())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
 
 # Application definition
